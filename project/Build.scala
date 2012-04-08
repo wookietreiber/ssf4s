9254b6ed026975485b1b0ce4374b973721607a09
Christian Krause <kizkizzbangbang@googlemail.com>
2012-04-08 14:20:07 +0200

    build and version update

--- project/Build.scala
+++ project/Build.scala
@@ -1,32 +1,21 @@
 import sbt._
 import Keys._
 
-import Dependencies._
-import BuildSettings._
-
-object BuildSettings {
-  val buildOrganization = "com.github.ssf4s"
-  val buildVersion      = "nightly"
-  val buildScalaVersion = "2.9.1"
-  val latest            = "latest.integration"
-
-  val buildSettings = Defaults.defaultSettings ++ Seq (
-    organization         := buildOrganization,
-    version              := buildVersion,
-    scalaVersion         := buildScalaVersion
-  )
-}
-
 object ssf4sBuild extends Build {
-  lazy val root = Project ( "ssf4s", file ("."),
-    settings = buildSettings ++ Seq (
-      libraryDependencies ++= Seq ( time, specs2 ),
-      initialCommands in Compile in console := """import scalax.ssf4s._"""
+  lazy val root = Project (
+    id        = "ssf4s",
+    base      = file ("."),
+    settings  = Defaults.defaultSettings ++ Seq (
+      organization         := "com.github.ssf4s",
+      version              := "0.2.0-SNAPSHOT",
+      scalaVersion         := "2.9.1",
+      libraryDependencies ++= Seq (
+        "org.scala-tools.time" %% "time"   % "0.5",
+        "org.specs2"           %% "specs2" % "1.9" % "test"
+      ),
+      initialCommands in Compile in console += """
+        import scalax.ssf4s._
+      """
     )
   )
-}
-
-object Dependencies {
-  lazy val time   = "org.scala-tools.time" %% "time"   % "0.5"
-  lazy val specs2 = "org.specs2"           %% "specs2" % "1.7.1" % "test"
 }
